63e7bb6321ebec018fc343679d843ab4db3dac56
Thái Nhật Minh <t.n.minh1997@gmail.com>
2026-06-09 22:06:17 +0700

    Revert "* DUmmy"
    This reverts commit 4e29fe7e535e3e2bdfb3f1c2a5b444ac914ec19a.

--- sdk/angelscript/source/as_compiler.cpp
+++ sdk/angelscript/source/as_compiler.cpp
@@ -11269,13 +11269,7 @@ int asCCompiler::CompileVariableAccess(const asCString &name, const asCString &s
 				Dereference(ctx, true);
 			}
 
-			// Adjust the pointer for composite member
-			// This is the same as what is in CompileExpressionPostOp
-			if( prop->compositeOffset || prop->isCompositeIndirect )
-				ctx->bc.InstrSHORT_DW(asBC_ADDSi, (short)prop->compositeOffset, engine->GetTypeIdFromDataType(dt));
-			if (prop->isCompositeIndirect)
-				ctx->bc.Instr(asBC_RDSPtr);
-
+			// TODO: This is the same as what is in CompileExpressionPostOp
 			// Put the offset on the stack
 			ctx->bc.InstrSHORT_DW(asBC_ADDSi, (short)prop->byteOffset, engine->GetTypeIdFromDataType(dt));
 
